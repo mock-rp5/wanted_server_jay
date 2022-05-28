59a6f1ab3da6fa1ca892a5d64be332b9f78bb800
yoo2wo <wogud758787@naver.com>
2022-05-29 00:23:13 +0900

    링크 추가, 조회, 삭제 수정 기능 추가

---
+++ src/main/java/com/example/demo/src/resume/model/PostLinkRes.java
@@ -0,0 +1,12 @@
+package com.example.demo.src.resume.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class PostLinkRes {
+    private long linkId;
+}
